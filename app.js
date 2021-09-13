ff1373b03c50af07df114a10c126a88d2a24fed2
ohien stephen <ohienstephen@gmail.com>
2021-09-13 16:18:10 +0100

    fixed  in lat and lng in mapbox

--- app.js
+++ app.js
@@ -30,8 +30,19 @@ function loadData(ip){
         location_output.innerText = `${data.location.city}, ${data.location.country}`
         timezone_output.innerText = 'UTC ' + data.location.timezone
         isp_output.innerText = data.isp
+
+        var mymap = L.map('map').setView([data.location.lat, data.location.lng], 13);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: 'pk.eyJ1Ijoic3RlcGhlbjAwOSIsImEiOiJja3RpcnVnaDQxNHJqMnltcmJraGFvM2htIn0.SWLQVfB6DGwLTRy7LX79Mw'
+        }).addTo(mymap);
     })
     .catch(err => console.error(err))
+
 }
 
 loadData()
